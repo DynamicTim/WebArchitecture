9477cd7b55af6c6818c1c8b5bf10aaae7a8f125b
Jacob Fisher <zombiefish90@gmail.com>
2016-02-21 17:12:20 -0700

    Sign in and Sign up pages are in place.

--- ZombieUniversity/config/routes.rb
+++ ZombieUniversity/config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :courses
   resources :sections
   resources :professors
   resources :students
+  resources :users
 	
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+    root 'home#landing'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
