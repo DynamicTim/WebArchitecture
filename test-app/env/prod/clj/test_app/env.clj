fabd8e6badb1b9ab31955679b282cfd2928418ba
Jacob Fisher <zombiefish90@gmail.com>
2016-05-09 11:36:17 -0600

    commits for backbone todo app

--- test-app/env/prod/clj/test_app/env.clj
+++
@@ -1,8 +0,0 @@
-(ns test-app.env
-  (:require [clojure.tools.logging :as log]))
-
-(def defaults
-  {:init
-   (fn []
-     (log/info "\n-=[test-app started successfully]=-"))
-   :middleware identity})
